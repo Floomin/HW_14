e56b16fda8b26084eca123c60735c18848644f39
Floomin <floomin@gmail.com>
2024-05-06 23:45:39 +0300

    Теперь 8 строк

--- main.js
+++ main.js
@@ -1,17 +1,9 @@
-let set = prompt('Введите ваши числа, раделяя запятыми: ');
-let mass = set.split(',').map((Number));
-
-if (mass.some(isNaN)) {
-    alert('Ошибка: Введите только числа, разделённые запятой!');
-}
+let set = prompt('Введите ваши числа, раделяя запятыми: '), mass = set.split(',').map((Number));
+if (mass.some(isNaN)) { alert('Ошибка: Введите только числа, разделённые запятой!'); }
 else {
     let sum = 0;
-    for (let i of mass) {
-        sum += i;
-
-    }
+    for (let i of mass) { sum += i; }
     let average = sum / mass.length;
-
     alert(`Среднее арифметическое ваших чисел ${average}`);
 }
 
